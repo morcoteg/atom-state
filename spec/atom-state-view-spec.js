e5600f51c909774573bba15fb32ba69081dbe6a4
morcoteg <gabrielmorcote@gmail.com>
2017-12-19 19:38:28 -0600

    initial package skeleton

---
+++ spec/atom-state-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomStateView from '../lib/atom-state-view';
+
+describe('AtomStateView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
